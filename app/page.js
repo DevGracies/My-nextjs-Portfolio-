6cd24f909cf76490c729ac5791d9a1e4dea424a1
Devgraices <graceadegunle16@gmail.com>
2025-02-18 19:58:45 +0100

    commit

--- app/page.js
+++ app/page.js
@@ -8,31 +8,29 @@ import Navigation from "./components/Navigation";
 import Projects from "./components/Projects";
 
 export default function Home() {
-  const [isDarkMode, setIsDarkMode] = useState(true);
-
-  useEffect(() => {
-    if (
-      localStorage.theme === "dark" ||
-      (!("theme" in localStorage) &&
-        window.matchMedia("(prefers-color-scheme: dark)").matches)
-    ) {
-      setIsDarkMode(true);
-    } else {
-      setIsDarkMode(false);
+  const [isDarkMode, setIsDarkMode] = useState(() => {
+    if (typeof window !== "undefined") {
+      return localStorage.getItem("theme") === "dark" ||
+        (!localStorage.getItem("theme") &&
+          window.matchMedia("(prefers-color-scheme: dark)").matches)
+        ? true
+        : false;
     }
-  }, []);
+    return true; // Default to dark mode
+  });
+
   useEffect(() => {
     if (isDarkMode) {
       document.documentElement.classList.add("dark");
-      localStorage.theme = "dark";
+      localStorage.setItem("theme", "dark");
     } else {
       document.documentElement.classList.remove("dark");
-      localStorage.theme = "";
+      localStorage.setItem("theme", "light");
     }
   }, [isDarkMode]);
 
   return (
-    <div className="relative justify-center overflow-hidden items-center flex sm:px-10 px-5 mx-auto flex-col">
+    <div className="relative flex flex-col items-center justify-center overflow-hidden px-5 sm:px-10 mx-auto">
       <Navigation isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} />
       <Header isDarkMode={isDarkMode} />
       <About isDarkMode={isDarkMode} />
