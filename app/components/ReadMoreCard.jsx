50c6ab529ba4730b19022bd74936036065a46072
Devgraices <graceadegunle16@gmail.com>
2025-04-07 10:19:14 +0100

    updated the cv

--- app/components/ReadMoreCard.jsx
+++ app/components/ReadMoreCard.jsx
@@ -3,13 +3,13 @@ import { useState } from "react";
 const ReadMoreCard = ({ text }) => {
   const [isExpanded, setIsExpanded] = useState(false);
   const words = text.split(" ");
-  const previewText = words.slice(0, 10).join(" ");
+  const previewText = words.slice(0, 15).join(" ");
 
   return (
     <div>
       <p>
         {isExpanded ? text : `${previewText} `}
-        {words.length > 10 && (
+        {words.length > 15 && (
           <button
             onClick={() => setIsExpanded(() => !isExpanded)}
             className="text-blue-500 hover:underline"
