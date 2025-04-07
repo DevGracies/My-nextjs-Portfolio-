8125ae42b81f8663ab7bcadaeb9f537566404f89
Devgraices <graceadegunle16@gmail.com>
2025-04-07 10:43:16 +0100

    updated the cv

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -52,7 +52,7 @@ const Header = () => {
           initial={{ y: 30, opacity: 0 }}
           whileInView={{ y: 0, opacity: 1 }}
           transition={{ duration: 0.6, delay: 1.2 }}
-          href="/Grace_Adegunle_Resume"
+          href="/Grace_Adegunle_Resume.pdf"
           download="/Grace_Adegunle_Resume.pdf"
           className="px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 "
         >
